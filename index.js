ff80fc5285ab0afe7711f9bde22bfc30a0588037
Oyinade3 <oyinadehillary@gmail.com>
2022-09-18 21:25:29 +0100

    js for game

--- index.js
+++ index.js
@@ -1,68 +1,77 @@
 /*
-player inputs choice between rock, paper and scissors
-computer makes a random choice between rock, paper and scissors
-a string should be rerturned to indicate whether player loses or wins
-the game should be played in 5 rounds and the one with the most wins is the overall winner
-*/
+whichever choice button the player clicks should trigger the game,
+computer choice and then the winner for each round should be
+displayed,
+the winner should be whoever scores 5 first
+ */
+function game(){
+    let choices = ["Rock", "Paper", "Scissors"];
+    //getting elements from DOM
+    let rock = document.querySelector('#rock');
+    let paper = document.querySelector('#paper');
+    let scissors = document.querySelector('#scissors');
+    let statement = document.querySelector('#statement');
+    let playerscore = document.querySelector("#playerscore");
+    let computerScore = document.querySelector("#computerScore");
+    let scoreContainer = document.querySelector(".score-container");
+    let buttons = [rock, paper, scissors]
 
 
-// function game(player, computer){
-        
-// }
-
-
-let playerWins = 0;
-let computerWins = 0;
-
-function compareChoices(player, computer, choiceArray){
-    if (player === computer) {
-      alert("It's a draw");
-    } else if(player === choiceArray[0]) {
-        if(computer === choiceArray[1]){
-            alert('you lost!, paper covers rock.');
-            computerWins += 1;
-        }else{
-            alert('you won!, rock smashes scissors.');
-            playerWins += 1;
-        };
-    } else if(player === choiceArray[1]){
-        if(computer === choiceArray[2]){
-            alert('you lost!, scissors cuts paper');
-            computerWins += 1;
-        }else{
-            alert('you win!, paper covers rock.')
-            playerWins += 1;
+    let playerWins = 0;
+    let computerWins = 0;
+    function startGame(player, computer, choiceArray) {
+      if (player === computer) {
+        statement.textContent = "it's a draw!";
+      } else if (player === choiceArray[0]) {
+        if (computer === choiceArray[1]) {
+          statement.textContent = "you lost!, paper covers rock.";
+          computerScore.innerHTML = computerWins += 1;
+        } else {
+          statement.textContent = "you won!, rock smashes scissors.";
+          playerscore.innerHTML = playerWins += 1;
         }
-    } else if(player === choiceArray[2]){
-        if (computer === choiceArray[0]){
-            alert('you lost!, rock smashes scissors.')
-            computerWins += 1;
-        }else{
-            alert("you won!, scissors cuts paper");
-            playerWins += 1;
+      } else if (player === choiceArray[1]) {
+        if (computer === choiceArray[2]) {
+          statement.textContent = "you lost!, scissors cuts paper";
+          computerScore.innerHTML = computerWins += 1;
+        } else {
+          statement.textContent = "you win!, paper covers rock.";
+          playerscore.innerHTML = playerWins += 1;
         }
+      } else if (player === choiceArray[2]) {
+        if (computer === choiceArray[0]) {
+          statement.textContent = "you lost!, rock smashes scissors.";
+          computerScore.innerHTML = computerWins += 1;
+        } else {
+          statement.textContent = "you won!, scissors cuts paper";
+          playerscore.innerHTML = playerWins += 1;
+        }
+      }
+      console.log(computer);
+      gameOver(playerWins,computerWins);
+    }
+
+    // function that triggers game on each click of a button
+    buttons.forEach((button) =>{
+        button.addEventListener("click", () =>{
+            let computerRandom = Math.floor(Math.random() * choices.length);
+            let computerChoice = choices[computerRandom];
+            startGame(button.innerText, computerChoice, choices);
+        });})
+    
+        // function for determining who scores 5 first
+    function gameOver(playerWin, computerWin){
+        let reloadbtn = document.querySelector('.reload');
+
+        if (playerWin === 5 && playerWin > computerWin){
+            statement.textContent = 'You Win!!'
+            reloadbtn.style.display = 'block';
+        } else if(computerWin === 5 && computerWin > playerWin){
+            statement.textContent = "You lose!!";
+            reloadbtn.style.display = "block";
+        }
+        reloadbtn.addEventListener('click', ()=>{
+            window.location.reload()})
     }
 }
-function startGame(){
-    let roundCount = parseInt(prompt('How many rounds do you want to play?\n(in numbers)'))
-    for (let i = 0; i<roundCount; i++){
-        let choices = ["rock", "paper", "scissors"];
-        let playerSelection = prompt("Rock, Paper or Scissors?");
-        let playerChoice = playerSelection.trim().toLowerCase();
-        let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-        compareChoices(playerChoice, computerChoice, choices);
-    }
-    if (playerWins > computerWins){
-        alert('you won!!!\nreload to play again')
-    }else if(playerWins === computerWins){
-    alert("it was a draw!!\nreload to try again");
-    } else{
-        alert("alas, you lost!\nreload to try again");
-    }
-    console.log(computerChoice);
-    console.log(playerWins, computerWins)
-}
-
-    console.log(playerWins, computerWins);
-
-startGame()
+game()
